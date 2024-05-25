e6d2de0c5b7b82f668bea37a52d8bc7b520f847c
Nathan McCutchen <n8wm@icloud.com>
2024-05-24 18:18:56 -0700

    app files

---
+++ ResumeGen/ResumeEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  ResumeEditorView.swift
+//  ResumeGen
+//
+//  Created by Nathan McCutchen on 5/24/24.
+//
+
+import SwiftUI
+
+struct ResumeEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResumeEditorView()
+}
---
+++ ResumeGen/ResumeGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeGenApp.swift
+//  ResumeGen
+//
+//  Created by Nathan McCutchen on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
